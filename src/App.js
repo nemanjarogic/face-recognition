a127a1f0a8cd1c124510aabf32761541d7488377
nemanjarogic <nemanjarogic93@gmail.com>
2019-10-12 18:01:09 +0200

    Styling, navigation and logo components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      <Logo />
     </div>
   );
 }
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import logo from './logo.png';
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 120, width: 120 }} >
+                <div className="Tilt-inner pa1">  
+                    <img alt='Face Recognition Logo' src={logo} />
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
