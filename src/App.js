266975c0d921db052db70918bac29b85fe01cbe0
nemanjarogic <nemanjarogic93@gmail.com>
2019-10-12 19:04:01 +0200

    Particles page animation and frontend changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesConfiguration = {
+  "particles": {
+      "number": {
+          "value": 100
+      },
+      "size": {
+          "value": 2
+      }
+  },
+  "interactivity": {
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "repulse"
+          }
+      }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={particlesConfiguration}/>
       <Navigation />
       <Logo />
+      <Rank />
+      <ImageLinkForm />
     </div>
   );
 }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,9 +5,9 @@ import logo from './logo.png';
 
 const Logo = () => {
     return (
-        <div className="ma4 mt0">
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 120, width: 120 }} >
-                <div className="Tilt-inner pa1">  
+        <div className='ma4 mt0 center'>
+            <Tilt className='Tilt br2 shadow-2' options={{ max : 50 }} style={{ height: 120, width: 120 }} >
+                <div className='Tilt-inner pa1'>  
                     <img alt='Face Recognition Logo' src={logo} />
                 </div>
             </Tilt>
