4db26a40dd5005a93e700d26221eae0781c880ed
nemanjarogic <nemanjarogic93@gmail.com>
2019-10-21 23:49:52 +0200

    Connecting front-end with server API

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
      .then(response => {
        if(response) {
-         fetch('http://localhost:3000/image', {
+         fetch('http://localhost:3001/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
